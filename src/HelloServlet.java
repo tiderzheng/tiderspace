47606ce7112ef8abe2ae2fa2cc0a2411be04c84a
Tider <tiderzheng@gmail.com>
2019-12-26 19:35:13 +0800

    test

---
+++ src/HelloServlet.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    int t;
+
+    public int getT() {
+        return t;
+    }
+
+    public void setT(int t) {
+        this.t = t;
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
